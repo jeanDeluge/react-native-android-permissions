99a075fe4868db559ece2e323232bb12005478bf
jeanDeluge <53405354+jeanDeluge@users.noreply.github.com>
2021-07-04 17:15:49 +0900

    feat: overlay permission

--- android/src/main/java/com/reactnativeandroidpermissions/AndroidPermissionsModule.java
+++ android/src/main/java/com/reactnativeandroidpermissions/AndroidPermissionsModule.java
@@ -1,19 +1,53 @@
 package com.reactnativeandroidpermissions;
 
 import androidx.annotation.NonNull;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.modules.permissions.PermissionsModule;
 
 @ReactModule(name = AndroidPermissionsModule.NAME)
 public class AndroidPermissionsModule extends ReactContextBaseJavaModule {
     public static final String NAME = "AndroidPermissions";
 
+    private final ReactApplicationContext reactContext;
+    private final int DRAW_OVER_PERMISSION_REQUEST_CODE = 123;
+    private Promise mPromise;
+    private final String error  = "Permission was not granted";
+
+    private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener(){
+        @Override
+        public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data){
+            super.onActivityResult(activity, requestCode, resultCode, data);
+            if(requestCode == DRAW_OVER_PERMISSION_REQUEST_CODE){
+                if (Settings.canDrawOverlays(activity.getApplicationContext())) {
+                    mPromise.resolve(true);
+                }
+                else {
+                    mPromise.reject(new Throwable(error));
+                }
+            }else{
+                mPromise.resolve(true);
+            }
+        }
+    };
+
     public AndroidPermissionsModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        this.reactContext = reactContext;
+
+        this.reactContext.addActivityEventListener(mActivityEventListener);
     }
 
     @Override
@@ -22,13 +56,15 @@ public class AndroidPermissionsModule extends ReactContextBaseJavaModule {
         return NAME;
     }
 
-
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
+    public void overlaypermission(Promise promise){
+        mPromise = promise;
+        if (!Settings.canDrawOverlays(this.reactContext)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + this.reactContext.getPackageName()));
+            this.reactContext.startActivityForResult(intent, DRAW_OVER_PERMISSION_REQUEST_CODE, null);
+        } else {
+            promise.resolve(true);
+        }
     }
 
-    public static native int nativeMultiply(int a, int b);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import { NativeModules } from 'react-native';
 
-type AndroidPermissionsType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
 const { AndroidPermissions } = NativeModules;
 
-export default AndroidPermissions as AndroidPermissionsType;
+export default AndroidPermissions;
