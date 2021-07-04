aebcd5c1102c3a59e9b50edabcb6371d1781fbaa
jeanDeluge <53405354+jeanDeluge@users.noreply.github.com>
2021-07-04 20:16:31 +0900

    Feat/check permission (#5)
    * feat: setting android env and modifiy gradle version 6.2 to 6.3-all
    
    * feat: overlay permission
    
    * fix: import android.content.pm.PackageManager
    
    * fix: requestPermission

--- android/src/main/java/com/reactnativeandroidpermissions/AndroidPermissionsModule.java
+++ android/src/main/java/com/reactnativeandroidpermissions/AndroidPermissionsModule.java
@@ -10,7 +10,6 @@ import android.os.Process;
 import android.content.Context;
 import android.content.pm.PackageManager;
 
-
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.BaseActivityEventListener;
 
@@ -29,8 +28,6 @@ public class AndroidPermissionsModule extends ReactContextBaseJavaModule {
     private final int DRAW_OVER_PERMISSION_REQUEST_CODE = 123;
     private Promise mPromise;
     private final String error  = "Permission was not granted";
-
-
 
     private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener(){
         @Override
@@ -74,6 +71,51 @@ public class AndroidPermissionsModule extends ReactContextBaseJavaModule {
         }
         promise.resolve(context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
     }
+  
+    @ReactMethod
+    public void requestPermission(final String permission, final Promise promise) {
+        Context context = getReactApplicationContext().getBaseContext();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+          promise.resolve(
+              context.checkPermission(permission, Process.myPid(), Process.myUid())
+                      == PackageManager.PERMISSION_GRANTED
+                  ? GRANTED
+                  : DENIED);
+          return;
+        }
+        if (context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+          promise.resolve(GRANTED);
+          return;
+        }
+    
+        try {
+          PermissionAwareActivity activity = getPermissionAwareActivity();
+    
+          mCallbacks.put(
+              mRequestCode,
+              new Callback() {
+                @Override
+                public void invoke(Object... args) {
+                  int[] results = (int[]) args[0];
+                  if (results.length > 0 && results[0] == PackageManager.PERMISSION_GRANTED) {
+                    promise.resolve(GRANTED);
+                  } else {
+                    PermissionAwareActivity activity = (PermissionAwareActivity) args[1];
+                    if (activity.shouldShowRequestPermissionRationale(permission)) {
+                      promise.resolve(DENIED);
+                    } else {
+                      promise.resolve(NEVER_ASK_AGAIN);
+                    }
+                  }
+                }
+              });
+    
+          activity.requestPermissions(new String[] {permission}, mRequestCode, this);
+          mRequestCode++;
+        } catch (IllegalStateException e) {
+          promise.reject(ERROR_INVALID_ACTIVITY, e);
+        }
+      }
 
     @ReactMethod
     public void overlaypermission(Promise promise){
